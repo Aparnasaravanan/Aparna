e977c15054d036609839de45a226fc9f3c908104
Aparnasaravanan <41137008+Aparnasaravanan@users.noreply.github.com>
2018-07-12 10:28:42 +0530

    Add files via upload

---
+++ OddEven.java
@@ -0,0 +1,14 @@
+package prpfirst;
+import java.util.*;
+public class OddEven {
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	if(n%2!=0){
+		System.out.println("Odd");
+	}
+	else{
+		System.out.println("Even");
+	}
+	}
+}
