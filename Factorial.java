976c2784341ca3811a7bf52f85d455448cae224c
Aparnasaravanan <41137008+Aparnasaravanan@users.noreply.github.com>
2018-07-14 18:36:30 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Factorial {
+	public static void main(String[] args){
+		 Scanner sc=new Scanner(System.in);
+		 int n,i,fact=1;
+		 n=sc.nextInt();
+		 for(i=1;i<n;i++){
+			 fact=fact+fact*i;
+		 }
+		 System.out.println(fact);
+		 
+		 
+	}
+}
