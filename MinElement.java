6a0e1c61b18d17d18e8e6b5d8f8e6a518818d2ee
Aparnasaravanan <41137008+Aparnasaravanan@users.noreply.github.com>
2018-07-15 11:15:16 +0530

    Add files via upload

---
+++ MinElement.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class MinElement {
+	public static void main(String[ ]args){
+		Scanner sc=new Scanner(System.in);
+		int n,i,min;
+		n=sc.nextInt();
+		int[] a=new int[n];
+		for(i=0;i<n;i++){
+			a[i]=sc.nextInt();
+		}
+		min=a[0];
+		for(i=0;i<n;i++){
+	
+			if(min>a[i]){
+				min=a[i];
+			}
+		}
+		System.out.println(min);
+		
+}
+}
