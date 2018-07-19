2989cb1d0a22b72a0035eb33ddd98c5fb1e1a873
Aparna <41137008+Aparnasaravanan@users.noreply.github.com>
2018-07-19 12:23:11 +0530

    Add files via upload

---
+++ StrAppend.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+public class StrAppend {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		StringBuffer s=new StringBuffer(str);
+		s.append(".");
+		System.out.println(s);	
+	}
+
+}
