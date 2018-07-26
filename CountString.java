61fd9ef5e237f6a7697a0dca060b9ac212f4e995
Aparna <41137008+Aparnasaravanan@users.noreply.github.com>
2018-07-26 18:31:07 +0530

    Add files via upload

---
+++ CountString.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class CountString {
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		String s;
+		s=sc.nextLine();
+		String[] s1=s.split(" ");
+		int n=s1.length;
+		System.out.println(n);
+	}
+
+}
