a272b3a0ebcfb235139593554629abec2a8cf7e8
Aparna <41137008+Aparnasaravanan@users.noreply.github.com>
2018-07-26 19:05:56 +0530

    Add files via upload

---
+++ NoOfLines.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class NoOfLines {
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		String s;
+		s=sc.nextLine();
+		String[] s1=s.split("\\.");
+		System.out.println(s1.length+"er");
+		if(s1.length==1){
+			System.out.println(1);
+		}else{
+			int n=s1.length-1;
+		System.out.println(n);
+	}
+	}
+
+}
